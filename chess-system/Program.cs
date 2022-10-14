0b9b22a457c60818bd15cfc1cff8031a705ebb61
Aluisio Anderson <aaluisio@gmail.com>
2022-10-14 10:58:54 -0300

    Fisrt class: Position

--- chess-system/Program.cs
+++ chess-system/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using tabuleiro;
 
 namespace chess_system
 {
@@ -10,6 +7,13 @@ namespace chess_system
     {
         static void Main(string[] args)
         {
+            
+            Posicao P;
+
+            P = new Posicao(3, 4);
+
+            //Testar antes de criar o ToString na classe Posicao
+            Console.WriteLine("Posição: " + P);
         }
     }
 }
