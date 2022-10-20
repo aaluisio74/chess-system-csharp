32230edd27e6aab6da44016add04d69f25a2f967
Aluisio Anderson <aaluisio@gmail.com>
2022-10-20 14:46:25 -0300

    Handling custom exception errors

--- chess-system/Program.cs
+++ chess-system/Program.cs
@@ -9,14 +9,20 @@ namespace chess_system
     {
         static void Main(string[] args)
         {
+            try
+            {
+                Board board = new Board(8,8);
 
-            Board board = new Board(8,8);
+                board.placePiece(new Rook(board, Color.Black), new Position(0, 0));
+                board.placePiece(new Rook(board, Color.Black), new Position(1, 3));
+                board.placePiece(new King(board, Color.Black), new Position(0, 2));
 
-            board.placePiece(new Rook(board, Color.Black), new Position(0, 0));
-            board.placePiece(new Rook(board, Color.Black), new Position(1, 3));
-            board.placePiece(new King(board, Color.Black), new Position(2, 4));
-
-            UI.printBoard(board);
+                UI.printBoard(board);
+            }
+            catch (BoardException e)
+            {
+                Console.WriteLine(e.Message);
+            }
 
             Console.ReadLine();
         }
--- chess-system/board/Board.cs
+++ chess-system/board/Board.cs
@@ -19,10 +19,45 @@ namespace board
             return pieces[row, column];
         }
 
+        public bool thereIsPiece(Position position)
+        {
+            validatePosition(position);
+            return piece(position) != null;
+        }
+
         public void placePiece(Piece piece, Position position)
         {
+            if (thereIsPiece(position))
+            {
+                throw new BoardException("There is already a piece on position!");
+            }
+
             pieces[position.row, position.column] = piece;
             piece.position = position;
+        }
+
+        public Piece piece(Position position)
+        {
+            return pieces[position.row, position.column];
+        }
+
+        //Verifica se a posição é válida
+        public bool validPosition(Position position) //posição Valida
+        {
+            if (position.row < 0 || position.row >= rows || position.column < 0 || position.column >= columns)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        //Validar a posição
+        public void validatePosition(Position position) //Validar posição
+        {
+            if (!validPosition(position))
+            {
+                throw new BoardException("Position not on the board. Invalid position!");
+            }
         }
     }
 }
