32df666501241041d127cda7b746e412d4f2ec82
Aluisio Anderson <aaluisio@gmail.com>
2022-10-21 08:34:34 -0300

    Creating the class: ChessPosition

--- chess-system/Program.cs
+++ chess-system/Program.cs
@@ -9,20 +9,11 @@ namespace chess_system
     {
         static void Main(string[] args)
         {
-            try
-            {
-                Board board = new Board(8,8);
+            ChessPosition position = new ChessPosition('c', 7);
 
-                board.placePiece(new Rook(board, Color.Black), new Position(0, 0));
-                board.placePiece(new Rook(board, Color.Black), new Position(1, 3));
-                board.placePiece(new King(board, Color.Black), new Position(0, 2));
+            Console.WriteLine(position);
 
-                UI.printBoard(board);
-            }
-            catch (BoardException e)
-            {
-                Console.WriteLine(e.Message);
-            }
+            Console.WriteLine(position.toPosition());
 
             Console.ReadLine();
         }
---
+++ chess-system/chess/ChessPosition.cs
@@ -0,0 +1,27 @@
+﻿using board;
+
+namespace chess
+{
+    class ChessPosition
+    {
+        public char column { get; set; }
+        public int row { get; set; }
+
+        public ChessPosition(char column, int row)
+        {
+            this.column = column;
+            this.row = row;
+        }
+
+        //Método para converter a posição do xadrez para a matriz interna.
+        public Position toPosition()
+        {
+            return new Position(8 - row, column - 'a');
+        }
+
+        public override string ToString()
+        {
+            return "" + column + row;
+        }
+    }
+}
